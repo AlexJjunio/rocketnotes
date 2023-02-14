0f391e64a45299196c65484bb04202c41fe96ed8
iJjunio <alex_jjunio@outlook.com>
2023-02-14 12:08:52 -0300

    I install css in js and applied

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./style"
+
+export function Details() {
+  return(
+    <Container>
+      <h1>Hello World!</h1>
+      <span>Alex Junio Moreira</span>
+    </Container>
+  )
+}
