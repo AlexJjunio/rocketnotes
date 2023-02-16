782a390b82f76265d0de5cd3473b4bfd7908199f
iJjunio <alex_jjunio@outlook.com>
2023-02-16 12:05:36 -0300

    I added the Header component in my application

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,11 @@
 import { Container } from "./style"
-
+import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 
 export function Details() {
   return(
     <Container>
-      <h1>Hello World!</h1>
-      <span>Alex Junio Moreira</span>
+      <Header/>
       <Button title="Entrar" loading />
       
     </Container>
--- src/pages/Details/style.js
+++ src/pages/Details/style.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: ;
+  display: grid;
+  grid-template-rows: 105px auto;
+  grid-template-areas: 
+  "header"
+  "content"
+  ;
+
+  width: 100%;
+  height: 100vh;
+
 `;
