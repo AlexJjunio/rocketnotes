d6e2be25fce3fca571feb5b2edd58017e237c31f
iJjunio <alex_jjunio@outlook.com>
2023-02-16 10:03:56 -0300

    I created a components folder containing a component button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,14 @@
 import { Container } from "./style"
 
+import { Button } from "../../components/Button"
+
 export function Details() {
   return(
     <Container>
       <h1>Hello World!</h1>
       <span>Alex Junio Moreira</span>
+      
+      <Button />
     </Container>
   )
 }
