da8c53a76c47e6710a471484ac384ffb978ddc5c
iJjunio <alex_jjunio@outlook.com>
2023-02-17 11:06:43 -0300

    Added header profile field

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,15 @@
-import { Container } from "./style";
+import { Container, Profile } from "./style";
 
 export function Header() {
   return(
     <Container>
-
+      <Profile>
+        <img src="https://github.com/alexjjunio.png" alt="Foto do usúario" />
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Alex Junio Moreira</strong>
+        </div>
+      </Profile>
     </Container>
   )
 }
