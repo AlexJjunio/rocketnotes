719f1b5541582d7bba65148739bfe23b631658b0
iJjunio <alex_jjunio@outlook.com>
2023-02-19 10:22:37 -0300

    I created the bookmarks

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./style"
 import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
 import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
 
 export function Details() {
   return(
@@ -20,7 +21,12 @@ export function Details() {
         </Links>
       </Section>
 
-      <Button title="Entrar" loading />
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="nodejs"/>
+      </Section>
+
+      <Button title="Voltar" />
       
     </Container>
   )
