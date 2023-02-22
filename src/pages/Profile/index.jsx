a0cf1c00ce07db681e874a2563fff9c347deb81f
iJjunio <alex_jjunio@outlook.com>
2023-02-22 08:26:57 -0300

    Profile edit page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,42 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi"
+
+import { Container, Form } from "./style";
+import { Input } from "../../components/Input"
+import { Button } from "../../components/Button"
+
+export function Profile() {
+  return(
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft/>
+        </a>
+      </header>
+
+      <Form>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        <Input
+          placeholder="Senha atual"
+          type="password"
+          icon={FiLock}
+        />
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar"/>
+      </Form>
+    </Container>
+  )
+}
