d39fdf86fd9b7db3cc9c12c38796b903e9907e78
iJjunio <alex_jjunio@outlook.com>
2023-02-22 09:51:39 -0300

    Creating new notes page header

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,11 +1,22 @@
-import { Container } from "./style";
+import { Container, Form } from "./style";
 
 import { Header } from "../../components/Header"
+import { Input } from "../../components/Input"
 
 export function New() {
   return(
     <Container>
-      <Header/>
+        <Header/>
+        
+      <main>
+          <Form>
+            <header>
+              <h1>Criar nota</h1>
+              <a href="/">voltar</a>
+            </header>
+            <Input placeholder="Título"/>
+          </Form>
+      </main>
     </Container>
   )
 }
--- src/pages/New/style.js
+++ src/pages/New/style.js
@@ -11,3 +11,24 @@ export const Container = styled.div`
   "content"
   ;
 `;
+
+export const Form = styled.form`
+  max-width: 550px;
+  margin: 38px auto;
+
+  > header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    margin-bottom: 36px;
+
+    h1 {
+    }
+
+    a {
+      font-size: 20px;
+      color: ${({theme}) => theme.COLORS.GRAY_100};
+    }
+  }
+`;
