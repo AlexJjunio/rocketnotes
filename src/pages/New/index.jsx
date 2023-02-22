d1230148f42af451b12ef643154f6a8f23cb324c
iJjunio <alex_jjunio@outlook.com>
2023-02-22 09:30:28 -0300

    Creating notes page structure

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./style";
+
+import { Header } from "../../components/Header"
+
+export function New() {
+  return(
+    <Container>
+      <Header/>
+    </Container>
+  )
+}
