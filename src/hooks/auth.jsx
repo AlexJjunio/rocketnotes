709a287b60a6c4f73ff7d399d4ea2f516369240d
iJjunio <alex_jjunio@outlook.com>
2023-03-16 12:04:31 -0300

    Using local storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { api } from "../services/api"
 
 export const AuthContext = createContext({});
@@ -12,6 +12,8 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", {email, password})
       const {user, token} = response.data;
 
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+      localStorage.setItem("@rocketnotes:token", token)
       
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({user, token})
@@ -23,6 +25,22 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@rocketnotes:token")
+    const user = localStorage.getItem("@rocketnotes:user")
+
+    if(token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+  
+      setData({
+        token,
+        user: JSON.parse(user)
+      })
+    }
+
+  }, [])
+
 
   return(
     <AuthContext.Provider value = {{singIn, user: data.user}}>
